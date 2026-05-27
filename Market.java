c579738807028a228f7edc0d30bce42731b47587
Your Name <you@example.com>
2026-05-27 20:00:20 +0530

    example

---
+++ Market.java
@@ -0,0 +1,7 @@
+class Market{
+	public static void main(String [] args){
+		System.out.println("****** Super Market ********");
+		System.out.print("1      Fruit      1000");
+		System.out.println("2     Apole        200");
+	}
+} 
